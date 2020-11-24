2d56f23288b906be92f055713a2840bb3d1ba55f
Leah Small <small.leah@gmail.com>
2020-11-24 16:05:24 +0800

    completed rails-task-manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new # needed to instantiate the form_for
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to root_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to root_path
+  end
+
+  # <Task id: nil, title: nil, details: nil, completed: false, created_at: nil, updated_at: nil>
+
+  # <Task id: 2, title: "Studying", details: "A lot of flashcards to do", completed: true, created_at: "2020-11-24 04:34:13", updated_at: "2020-11-24 04:34:13">
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to root_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
